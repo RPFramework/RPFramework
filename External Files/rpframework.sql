5f60a5d43885285fae55ce6e6dd2724c62c36565
Innovative Studios <innovativestudios@outlook.com>
2016-09-23 12:05:15 -0500

    Updated bin files with latest edits.

--- External Files/rpframework.sql
+++ External Files/rpframework.sql
@@ -20,6 +20,9 @@ SET time_zone = "+00:00";
 -- Database: `rpframework`
 --
 
+CREATE DATABASE IF NOT EXISTS rpframework;
+USE rpframework;
+
 -- --------------------------------------------------------
 
 --
