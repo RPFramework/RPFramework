30d3db88f7597f1195c45527f95eb8eebe939b85
superihippo <superihippo@gmail.com>
2017-07-16 11:01:00 +0300

    Updated scripts and binary

--- pack.bat
+++ pack.bat
@@ -2,12 +2,12 @@ SET pbomanager="C:\Program Files\PBO Manager v.1.4 beta\PBOConsole.exe"
 SET repo="C:\Users\Kerkkoh\Documents\GitHub\RPFramework\"
 SET armalocation="C:\Program Files (x86)\Steam\steamapps\common\Arma 3\"
 
-SET reposerver="%repo:~1, -1%rpf_server"
-SET repomission="%repo:~1, -1%RPFramework.Altis"
+SET reposerver="%repo:~1, -1%@RPF_Server\addons\rpf_server"
+SET repomission="%repo:~1, -1%rpframework.Altis"
 SET pboserver="%armalocation:~1, -1%@RPF_Server\Addons\rpf_server.pbo"
-SET pbomission="%armalocation:~1, -1%MPMissions\RPFramework.Altis.pbo"
+SET pbomission="%armalocation:~1, -1%MPMissions\rpframework.altis.pbo"
 SET bakserver="%armalocation:~1, -1%@RPF_Server\Addons\rpf_server.pbo.bak"
-SET bakmission="%armalocation:~1, -1%MPMissions\RPFramework.Altis.pbo.bak"
+SET bakmission="%armalocation:~1, -1%MPMissions\rpframework.altis.pbo.bak"
 
 %pbomanager% -pack %reposerver% %pboserver%
 del /F %bakserver%
