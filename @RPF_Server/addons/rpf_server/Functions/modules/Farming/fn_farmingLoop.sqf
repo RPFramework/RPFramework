69cb492e5f4efec5c79d52b2959d77946c935cae
Kerkkoh <superihippo@gmail.com>
2018-03-06 20:17:05 +0200

    New items, Cleanup & Small fixes/tweaks

--- @RPF_Server/addons/rpf_server/Functions/modules/Farming/fn_farmingLoop.sqf
+++ @RPF_Server/addons/rpf_server/Functions/modules/Farming/fn_farmingLoop.sqf
@@ -8,12 +8,14 @@ for "_i" from 0 to 1 step 0 do
 	{
 		_state = _x getVariable "state";
 		if (_state < 0) then {
+			// Grow plants at different speeds: There's a 50% chance for faster (0.2) growth.
+			_newState = _state + 0.1;
 			if ((floor random 2) == 1) then {
 				_newState = _state + 0.2;
-				_plantPos = getPos _x;
-				_x setVariable ["state", _newState];
-				_x setPos [_plantPos select 0, _plantPos select 1, _newState];
 			};
+			_plantPos = getPos _x;
+			_x setVariable ["state", _newState];
+			_x setPos [_plantPos select 0, _plantPos select 1, _newState];
 		};
 	}forEach RPF_plantArray;
 };
--- @RPFramework/addons/rpf_farming/config.cpp
+++ @RPFramework/addons/rpf_farming/config.cpp
@@ -5,17 +5,19 @@ class CfgPatches
 		units[] = {"RPF_Plant_Olive","RPF_Plant_Poppy"};
 		weapons[] = {};
 		requiredVersion = 0.1;
-		requiredAddons[] = {"RPF_General"};
+		requiredAddons[] = {"RPF_General","A3_Plants_F_Bush"};
 	};
 };
 class CfgVehicles {
 	class RPF_PhysBase;
-	class RPF_Plant_Olive: RPF_PhysBase {
+	class RPF_Plant_Olive: RPF_PhysBase
+	{
 		scope = 2;
 		model = "A3\plants_f\Bush\b_FicusC1s_F.p3d";
 		displayName = "$STR_RPF_FARMING_PLANTS_OLIVE";
 	};
-	class RPF_Plant_Poppy: RPF_PhysBase {
+	class RPF_Plant_Poppy: RPF_PhysBase
+	{
 		scope = 2;
 		model = "A3\plants_f\Bush\b_NeriumO2s_F.p3d";
 		displayName = "$STR_RPF_FARMING_PLANTS_POPPY";
